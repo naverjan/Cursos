ba2506526a621742876628d825be086f36b6bbb1
Andres Verjan <andresver2040@gmail.com>
2020-02-07 20:28:11 -0500

    Inclución de angular js en proyecto

---
+++ wwwroot/js/coursesController.js
@@ -0,0 +1,21 @@
+﻿var app = angular.module('myapp', ['ngRoute']);
+app.controller('myCtrl', ['$scope']);
+
+//app.config(["$routeProvider", function ($routeProvider) {
+//    $routeProvider
+//        .when('/ui-login', {
+//            templateUrl: 'ui-login.html',
+//            controller: 'loginController'
+//        })
+//        .otherwise({
+//            redirectTo: '/ui-login'
+//        });
+//}]);
+app.controller('loginController', function ($scope) {
+    //Controller Here
+    $scope.name = "Andres verjan";
+    $scope.submit = function () {
+        console.log("hi i m coming");
+    }
+    console.log("in controller");
+});
